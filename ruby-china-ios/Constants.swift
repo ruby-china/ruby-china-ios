cf43394ffd61b3e336894a43c1be2756e0b394f8
Jason Lee <huacnlee@gmail.com>
2016-08-22 17:10:04 +0800

    更新 Project URL

--- ruby-china-ios/Constants.swift
+++ ruby-china-ios/Constants.swift
@@ -10,7 +10,7 @@ let OAUTH_CLIENT_ID = "1b034acf"
 let OAUTH_SECRET = "2d44bae75daaa88f2b8226a0205318b6ccf79b09e80fbfb461d191001d7b3c7b"
 #endif
 
-let PROJECT_URL = "https://github.com/ruby-china/ruby-china-turbolinks"
+let PROJECT_URL = "https://github.com/ruby-china/ruby-china-ios"
 
 let USER_AGENT = "turbolinks-app, ruby-china, official"
 
