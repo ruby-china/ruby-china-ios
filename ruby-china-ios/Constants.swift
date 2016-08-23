53ad4fb7623b9fe4cae2a1dd29a033c57df1bafa
Jason Lee <huacnlee@gmail.com>
2016-08-23 18:22:28 +0800

    撤销会原来的红色导航;

--- ruby-china-ios/Constants.swift
+++ ruby-china-ios/Constants.swift
@@ -25,9 +25,9 @@ let BLACK_LIGHT = UIColor.grayColor()
 //let TABBAR_BG_COLOR = UIColor(red:0.88, green:0.96, blue:1.00, alpha:1.0)
 
 // Red Theme
-let PRIMARY_COLOR = UIColor(red: 0.90, green: 0.27, blue: 0.27, alpha: 1.0)
+let PRIMARY_COLOR = UIColor(red:0.91, green:0.33, blue:0.23, alpha:1.0)
 let NAVBAR_BG_COLOR = PRIMARY_COLOR
-let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red: 0.76, green: 0.13, blue: 0.17, alpha: 1.0)
+let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red:0.74, green:0.24, blue:0.13, alpha:1.0)
 let NAVBAR_BORDER_COLOR = UIColor(red:0.72, green:0.30, blue:0.26, alpha:1.0)
 let NAVBAR_TINT_COLOR = UIColor(red:1.00, green:1.00, blue:0.98, alpha:1.0)
 let SEGMENT_BG_COLOR = UIColor(red:0.23, green:0.05, blue:0.02, alpha:1.0)
