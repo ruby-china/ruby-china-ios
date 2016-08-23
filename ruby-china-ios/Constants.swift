b6fdc65e9dac43ac40792a5015a98b06d6ae18e7
Jason Lee <huacnlee@gmail.com>
2016-08-23 17:18:53 +0800

    ErrorView 重试按钮用主色调;

--- ruby-china-ios/Constants.swift
+++ ruby-china-ios/Constants.swift
@@ -16,7 +16,6 @@ let USER_AGENT = "turbolinks-app, ruby-china, official"
 
 let BLACK = UIColor(red:0.20, green:0.20, blue:0.20, alpha:1.0)
 let BLACK_LIGHT = UIColor.grayColor()
-let RED = UIColor(red:0.96, green:0.26, blue:0.21, alpha:1.0)
 
 // Blue Theme
 //let NAVBAR_BG_COLOR = UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.0)
@@ -26,8 +25,9 @@ let RED = UIColor(red:0.96, green:0.26, blue:0.21, alpha:1.0)
 //let TABBAR_BG_COLOR = UIColor(red:0.88, green:0.96, blue:1.00, alpha:1.0)
 
 // Red Theme
-let NAVBAR_BG_COLOR = UIColor(red:0.91, green:0.33, blue:0.23, alpha:1.0)
-let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red:0.74, green:0.24, blue:0.13, alpha:1.0)
+let PRIMARY_COLOR = UIColor(red: 0.90, green: 0.27, blue: 0.27, alpha: 1.0)
+let NAVBAR_BG_COLOR = PRIMARY_COLOR
+let SIDEMENU_NAVBAR_BG_COLOR = UIColor(red: 0.76, green: 0.13, blue: 0.17, alpha: 1.0)
 let NAVBAR_BORDER_COLOR = UIColor(red:0.72, green:0.30, blue:0.26, alpha:1.0)
 let NAVBAR_TINT_COLOR = UIColor(red:1.00, green:1.00, blue:0.98, alpha:1.0)
 let SEGMENT_BG_COLOR = UIColor(red:0.23, green:0.05, blue:0.02, alpha:1.0)
--- ruby-china-ios/Views/ErrorView.swift
+++ ruby-china-ios/Views/ErrorView.swift
@@ -7,6 +7,7 @@ class ErrorView: UIView {
     
     var error: Error? {
         didSet {
+            retryButton.tintColor = PRIMARY_COLOR
             titleLabel.text = error?.title
             messageLabel.text = error?.message
         }
