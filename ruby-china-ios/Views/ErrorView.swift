ba702f39fc3dc50df1a7e2239820ee0a242188ed
柯磊 <kelei0017@gmail.com>
2016-11-02 23:55:27 +0800

    错误提示的 retry 按钮文字支持双语

--- ruby-china-ios/Views/ErrorView.swift
+++ ruby-china-ios/Views/ErrorView.swift
@@ -8,6 +8,7 @@ class ErrorView: UIView {
     var error: Error? {
         didSet {
             retryButton.tintColor = PRIMARY_COLOR
+            retryButton.setTitle("retry".localized, for: .normal)
             titleLabel.text = error?.title
             messageLabel.text = error?.message
         }
