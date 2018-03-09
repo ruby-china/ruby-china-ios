ebdf19f72955c0adcad486bfe503172e905df6f1
柯磊 <kelei0017@gmail.com>
2018-03-09 23:05:06 +0800

    去掉 SideMenu 导航栏上的背景色

--- ruby-china-ios/Controllers/SideMenuNavigationController.swift
+++ ruby-china-ios/Controllers/SideMenuNavigationController.swift
@@ -11,11 +11,5 @@ import SideMenu
 class SideMenuNavigationController: UISideMenuNavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let bgFrame = CGRect(x: 0, y: -24, width: navigationBar.frame.width, height: navigationBar.frame.height + 24)
-        let bgView = UIView(frame: bgFrame)
-        
-        bgView.backgroundColor = SIDEMENU_NAVBAR_BG_COLOR
-        navigationBar.addSubview(bgView)
     }
 }
